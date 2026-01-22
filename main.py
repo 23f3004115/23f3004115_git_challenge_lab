ec2b16e7fdfae82614ae61a86b4fc42cd95c9f6e
23f3004115 <23f3004115@ds.study.iitm.ac.in>
2026-01-22 22:35:16 +0530

    Add main entry point for arithmetic operations

---
+++ main.py
@@ -0,0 +1,11 @@
+from sum_module import add
+from diff_module import subtract
+from prod_module import multiply
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    print("Addition:", add(a, b))
+    print("Difference:", subtract(a, b))
+    print("Product:", multiply(a, b))
